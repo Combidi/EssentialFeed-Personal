8c2990e24a6524875c9bac5550d1bd7d0b4270ce
Peter Combee <peter.combee@maxdoro.com>
2021-12-21 14:52:58 +0100

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Peter Combee on 21/12/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
