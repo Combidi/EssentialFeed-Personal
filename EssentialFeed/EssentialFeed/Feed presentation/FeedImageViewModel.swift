4550ea97802e4dbed0152f3c5e6d8ec0895691a8
Peter Combee <10615500+Combidi@users.noreply.github.com>
2022-03-21 10:18:56 +0100

    Move `FeedImageViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed presentation/FeedImageViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Peter Combee on 21/03/2022.
+//
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
