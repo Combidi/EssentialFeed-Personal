804b68ef7bd6cfaf2c2f02313342676206085864
Peter Combee <10615500+Combidi@users.noreply.github.com>
2022-03-21 08:49:17 +0100

    Move view models to separate files

---
+++ EssentialFeed/EssentialFeed/Feed presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  Created by Peter Combee on 21/03/2022.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeed/Feed presentation/FeedViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  Created by Peter Combee on 21/03/2022.
+//
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
