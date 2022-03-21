2774e4f12905d06ec2600d334151e431a1d4522e
Peter Combee <10615500+Combidi@users.noreply.github.com>
2022-03-21 08:54:41 +0100

    Replace `FeedPresenter` from iOS module with the `FeedPresenter` from the cross-platform EssentialFeed module

--- EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Created by Peter Combee on 01/03/2022.
-//
-
-struct FeedLoadingViewModel {
-    let isLoading: Bool
-}
--- EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Created by Peter Combee on 01/03/2022.
-//
-
-import EssentialFeed
-
-struct FeedViewModel {
-    let feed: [FeedImage]
-}
