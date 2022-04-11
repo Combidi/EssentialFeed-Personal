3f4b61aa84a9cb996d2d69a3577eec1e4e0232d6
Peter Combee <10615500+Combidi@users.noreply.github.com>
2022-04-11 20:50:56 +0200

    `CoreDataFeedStore.retrieveImageData` delivers stored data when there's an image with a matching URL in the store

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -3,14 +3,34 @@
 //
 
 import Foundation
+import CoreData
 
 extension CoreDataFeedStore: FeedImageDataStore {
     
     public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-        
+        perform { context in
+            guard let image = try? ManagedFeedImage.first(with: url, in: context) else { return }
+
+            image.data = data
+            try? context.save()
+        }
     }
     
     public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(.none))
+        perform { context in
+            completion(Result {
+                return try ManagedFeedImage.first(with: url, in: context)?.data
+            })
+        }
     }
+}
+
+extension ManagedFeedImage {
+    static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedFeedImage? {
+         let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
+         request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
+         request.returnsObjectsAsFaults = false
+         request.fetchLimit = 1
+         return try context.fetch(request).first
+     }
 }
--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -23,6 +23,16 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
     }
     
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() {
+        let sut = makeSUT()
+        let storedData = anyData()
+        let matchingURL = URL(string: "http://matching-url.com")!
+
+        insert(storedData, for: matchingURL, into: sut)
+    
+        expect(sut, toCompleteRetrievalWith: found(storedData), for: matchingURL)
+    }
+    
     // MARK: - Helpers
     private func makeSUT() -> CoreDataFeedStore {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
@@ -35,6 +45,10 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
            
     private func notFound() -> FeedImageDataStore.RetrievalResult {
         .success(.none)
+    }
+    
+    private func found(_ data: Data) -> FeedImageDataStore.RetrievalResult {
+        .success(data)
     }
     
     private func localImage(url: URL) -> LocalFeedImage {
