c3e2bde7f3258530c30978868ab890c32a58132d
Peter Combee <10615500+Combidi@users.noreply.github.com>
2022-06-05 21:22:35 +0200

    Duplicate `RemoteFeedLoader` as `RemoteImageCommentsLoader`

--- EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
@@ -1,17 +1,17 @@
 //
-//  Created by Peter Combee on 23/12/2021.
+//  Created by Peter Combee on 05/06/2022.
 //
 
 import Foundation
 
-internal final class FeedItemsMapper {
+internal final class ImageCommentsMapper {
     private struct Root: Decodable  {
         let items: [RemoteFeedItem]
     }
         
     internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            throw RemoteFeedLoader.Error.invalidData
+            throw RemoteImageCommentsLoader.Error.invalidData
         }
         
         return root.items
