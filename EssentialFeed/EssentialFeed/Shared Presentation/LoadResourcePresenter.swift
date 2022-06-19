63d335cafdf1dc75534fc74cc7f5e4c513d09eb7
Peter Combee <10615500+Combidi@users.noreply.github.com>
2022-06-19 21:21:03 +0200

    Move `GENERIC_CONNECTION_ERROR` localization key to new shared scope

--- EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -33,11 +33,12 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
         self.mapper = mapper
     }
         
-    private var feedLoadError: String {
-        return NSLocalizedString("GENERIC_CONNECTION_ERROR",
-            tableName: "Feed",
-            bundle: Bundle(for: FeedPresenter.self),
-            comment: "Error message displayed when we can't load the image feed from the server")
+    static public var loadError: String {
+        NSLocalizedString("GENERIC_CONNECTION_ERROR",
+            tableName: "Shared",
+            bundle: Bundle(for: Self.self),
+            comment: "Error message displayed when we can't load the resource from the server"
+        )
     }
 
     public func didStartLoading() {
@@ -51,7 +52,7 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
     }
     
     public func didFinishLoading(with error: Error) {
-        errorView.display(.error(message: feedLoadError))
+        errorView.display(.error(message: Self.loadError))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 }
