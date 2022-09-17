77670dc081899be3e1b72bc68e82997dc7f27ed5
Peter Combee <petercombee@gmail.com>
2022-09-17 09:54:16 +0200

    Move Feed URL creation to the Feed API module

---
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -0,0 +1,16 @@
+//
+//  Created by Peter Combee on 17/09/2022.
+//
+
+import Foundation
+
+public enum FeedEndpoint {
+    case get
+    
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case .get:
+            return baseURL.appendingPathComponent("/v1/feed")
+        }
+    }
+}
---
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -0,0 +1,18 @@
+//
+//  Created by Peter Combee on 17/09/2022.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedEndpointTests: XCTestCase {
+    
+    func test() {
+        let baseURL = URL(string: "http://base-url.com")!
+        
+        let received = FeedEndpoint.get.url(baseURL: baseURL)
+        let expected = URL(string: "http://base-url.com/v1/feed")
+        
+        XCTAssertEqual(received, expected)
+    }
+}
